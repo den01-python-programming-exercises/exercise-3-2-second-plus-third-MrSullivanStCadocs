d681026f671268ddd678aec9fec5bafba1967535
MrSullivan <>
2021-05-18 21:00:19 +0000

    list, while true with conditional break, sum recall from list and print command

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    myList = []
+    while True:
+      userNumber = int(input(""))
+      myList.append(userNumber)
+      if(userNumber == 0):
+        break
+    
+    sum = myList[1] + myList[2]
+    
+    print(str(sum))
+
+
+
 
 if __name__ == '__main__':
     main()
